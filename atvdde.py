2681152819119369e68cdfd81a91351874de183c
juliana <juliana.s.rodrigues7@aluno.senai.br>
2024-11-27 11:17:07 -0500

    a parte do calculo

--- atvdde.py
+++ atvdde.py
@@ -1,7 +1,5 @@
-print("digite a quantidade de aluno:")
-qntdaluno = int(input())
-print("digite o nome dos alunos:")
-nome = input()
+qntdAluno= input( "digite a quantidade de aluno:")
+nomes = input("digite o nome os alunos:")
 print("digite a nota dos alunos:")
 n1 = float(input())
 n2 = float(input())
@@ -11,10 +9,16 @@ print("digite a quantidade de faltas do aluno:")
 faltas = int(input())
 media = (n1+n2+n3+n4)/4
 if media >= 8:
-     print("O aluno esta aprovado com:",media)
-elif media >= 5:
-      print("O aluno esta de recuperação com:",media)
-elif media < 5:
-      print("O aluno esta reprovado com:",media)
+     print("O",nomes, "esta aprovado com:",media)
+else:
+      print("O",nomes,"esta reprovado e vai ter que fazer recuperação,por que tirou nota baixa",media)
+print("digite quanto o", "aluno tirou na recuperação")
+recuperacao = float(input())
+if recuperacao >= 5:
+      print("O aluno tirou",recuperacao ,"na recuperação")
+else:
+      print("o aluno não passou na recuperação,por que tirou",recuperacao)
+if recuperacao < 5:
+      print("O aluno esta reprovado por não alcançar a nota da recuperção e ficou com:",recuperacao)
 elif faltas > 31:
       print("o aluno esta reprovado com",faltas)
