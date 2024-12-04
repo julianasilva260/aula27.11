0a01f8dc5ca9d00a0dbfb757dc6f454b82a56d9c
juliana <juliana.s.rodrigues7@aluno.senai.br>
2024-12-04 10:42:56 -0500

    attvdd

--- revisao.py
+++ revisao.py
@@ -2,6 +2,7 @@
 cont = 0 #variavel que controla a reptição
 print("Digite a quantidade de alunos:")
 escolha_usuario = int(input()) #variavel que guarda quantas vezes o codigo vai rodar
+alunos = [] #lista que guardara todos os alunos cadrastados
 while cont < escolha_usuario:
     print("Digite o nome do aluno(a)")
     nome = input() #armazenar o nome do aluno
@@ -10,26 +11,27 @@ while cont < escolha_usuario:
     n2 =float(input())
     n3 =float(input())
     n4 =float(input()) 
+    print("Digite a quantidade de faltas do aluno(a)")
     faltas = int(input())
     #calculo da media 
     media = (n1+n2+n3+n4)/4
+    print(media)
     #LÓGICA DA SITUAÇÃO
     if faltas > 31:
         situacao = nome,"esta reprovado por faltas"
     elif media >= 8:
         situacao = nome,"esta aprovado"
     elif media >= 5: #recuperação
-         recuperacao = float(input()) #ler a nota da prova de recuperação
-         if recuperacao >= (10-media):
-            situacao= nome,"esta aprovado(a) na recuperação"
-         else:
-          situacao = nome,"esta reprovado(a) na recuperação"
+        recuperacao = float(input("Digite a nota do aluno(a) da recuperação:")) #ler a nota da prova de recuperação
+        if recuperacao >= (10-media):
+            situacao= "esta aprovado(a) na recuperação"
+        else:
+            situacao = "esta reprovado(a) na recuperação"
     else:
-        situacao = nome,"esta reprovado(a) por media com a nota"
+        situacao = "esta reprovado(a) por media"
+
+    #enviar os dados do aluno para a lista (alunos)
+    alunos.append([nome,faltas,media,situacao])
+    cont += 1
     #relatorio
-    print("RELATÓRIO DO ALUNO(A)",nome)
-    print("nome:",nome)
-    print("notas:",n1,n2,n3,n4)
-    print("faltas:",faltas)
-    print("situação do aluno(a):",situacao)
-    cont = cont+1
+print(alunos)
